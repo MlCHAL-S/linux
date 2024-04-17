5e55b2edf36af30fc9b63cc4a1e7254ee867abbf
M <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-04-17 20:49:55 +0200

    Tests not working

---
+++ java-test/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String helloWorld = "HellsdfsdoWorld!";
+        int a = 10;
+        System.out.println( helloWorld );
+        System.out.println( a );
+    }
+}
